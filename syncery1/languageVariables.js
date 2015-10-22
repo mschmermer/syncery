2b315051be55cac51329cccd179bb20ff16dc174
mschmermer <ma.schmermer@googlemail.com>
2015-10-22 11:28:16 +0700

    aktueller Stand

---
+++ syncery1/languageVariables.js
@@ -0,0 +1,14 @@
+angular.module('syncery')
+
+    .constant({
+        languageVariables: {
+            de: {
+                change_booking: "Buchung ändern",
+                show_booking: "Buchung anzeigen"
+            },
+            en: {
+                change_booking: "change booking",
+                show_booking: "show booking"
+            }
+        }
+    });
