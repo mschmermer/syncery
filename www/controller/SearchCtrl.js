271a0c0ad772675281bba47ae5eb2147415e28ea
mschmermer <ma.schmermer@googlemail.com>
2015-12-15 09:53:59 +0100

    Suche bar in header

--- www/controller/SearchCtrl.js
+++ www/controller/SearchCtrl.js
@@ -5,9 +5,20 @@
         .controller('SearchCtrl', SearchCtrl);
 
 
-    function SearchCtrl($scope) {
+    function SearchCtrl($scope, $ionicNavBarDelegate) {
         $scope.data = {};
         $scope.data.showSearch = true;
         $scope.data.searchQuery = '';
+
+        $scope.hide_navbar = false;
+
+        $scope.search = function(){
+            $scope.hide_navbar = true;
+        }
+
+        $scope.cancel = function(){
+            $scope.hide_navbar = false;
+        }
+
     }
 }());
